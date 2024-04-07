e41cdb5e04b976b69b4d1787a1308e45ca1fc3e7
Luis David <david12.29@hotmail.com>
2024-04-07 18:03:37 -0500

    Feat(back and Fronted) index

---
+++ src/main/java/com/graphql/example/conf/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.graphql.example.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("http://127.0.0.1:5500"); // Reemplaza con la URL de tu archivo HTML
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/graphql/**", config); // Reemplaza con la ruta de tu endpoint GraphQL
+        return new CorsFilter(source);
+    }
+}
