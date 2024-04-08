e0441e9b36844e3c8a5910761f3947ebb164ca75
Luis David <david12.29@hotmail.com>
2024-04-08 15:11:10 -0500

    Feat(back and fronted) I am happy

---
+++ src/main/resources/static/js/demo/chart-pie-demo.js
@@ -0,0 +1,145 @@
+// Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#858796';
+
+// Pie Chart Example
+// var ctx = document.getElementById("myPieChart");
+// var myPieChart = new Chart(ctx, {
+//   type: 'doughnut',
+//   data: {
+//     labels: ["null", "Referral", "Social"],
+//     datasets: [{
+//       data: [55, 30, 15],
+//       backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
+//       hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
+//       hoverBorderColor: "rgba(234, 236, 244, 1)",
+//     }],
+//   },
+//   options: {
+//     maintainAspectRatio: false,
+//     tooltips: {
+//       backgroundColor: "rgb(255,255,255)",
+//       bodyFontColor: "#858796",
+//       borderColor: '#dddfeb',
+//       borderWidth: 1,
+//       xPadding: 15,
+//       yPadding: 15,
+//       displayColors: false,
+//       caretPadding: 10,
+//     },
+//     legend: {
+//       display: false
+//     },
+//     cutoutPercentage: 80,
+//   },
+// });
+
+
+loadDiagram();
+
+async function loadDiagram(){
+
+  try {
+      const queryCourse = `
+          query {
+              findAllCourses {
+                  id,
+                  name,
+              }
+          }
+      `;
+      
+      const requestCourseDiagram = await fetch('http://localhost:8080/graphql', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+              'Accept': 'application/json',
+          },
+          body: JSON.stringify({ query: queryCourse })
+      })
+      let dataCourse = await requestCourseDiagram.json();
+      
+      
+      const query = `
+          query {
+              findAllStudents{
+                  course {
+                    id
+                  }
+              }
+          }
+      `;
+      
+      const requestStudent = await   fetch('http://localhost:8080/graphql', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+              'Accept': 'application/json',
+          },
+          body: JSON.stringify({ query })
+      })
+      let dataStudent = await requestStudent.json();
+      console.log(dataStudent.data);
+
+      let couserName=[];
+      let couserCount=[];
+      for (let element of dataCourse.data.findAllCourses) {
+          couserName.push(element.name);
+          couserCount.push({id: element.id,count:0});
+
+      }
+      for (let element of dataStudent.data.findAllStudents) {
+          let idCourse=element.course.id;
+          for (let elementCourse of couserCount){
+              
+              if(idCourse==elementCourse.id){
+                  elementCourse.count++;
+              }
+          }
+
+      }
+      console.log(couserCount);
+      let dataCourseChart=[];
+      for (let element of couserCount){
+        dataCourseChart.push(element.count);
+      }
+      console.log(dataCourseChart);
+
+      var ctx = document.getElementById("myPieChart");
+var myPieChart = new Chart(ctx, {
+  type: 'doughnut',
+  data: {
+    labels: couserName,
+    datasets: [{
+      data: dataCourseChart,
+      backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
+      hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
+      hoverBorderColor: "rgba(234, 236, 244, 1)",
+    }],
+  },
+  options: {
+    maintainAspectRatio: false,
+    tooltips: {
+      backgroundColor: "rgb(255,255,255)",
+      bodyFontColor: "#858796",
+      borderColor: '#dddfeb',
+      borderWidth: 1,
+      xPadding: 15,
+      yPadding: 15,
+      displayColors: false,
+      caretPadding: 10,
+    },
+    legend: {
+      display: false
+    },
+    cutoutPercentage: 80,
+  },
+});
+
+     
+      
+  } catch (error) {
+      
+      console.error('Error:', error);
+  }
+}
---
+++ src/main/resources/static/js/tablesProject.js
@@ -0,0 +1,311 @@
+// import { createStudent } from './graphqlFunctions.js';
+loadStudents();
+loadCourse();
+
+
+async function loadStudents(){
+
+    try {
+        const query = `
+            query {
+                findAllStudents {
+                    id
+                    name
+                    lastName
+                    age
+                    email,
+                    lastName
+                    course {
+                        id
+                        name
+                        category
+                        teacher
+                        description
+                    }
+                }
+            }
+        `;
+        
+        const requestStudent = await  fetch('http://localhost:8080/graphql', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            },
+            body: JSON.stringify({ query })
+        })
+        let dataStudent = await requestStudent.json();
+        
+        let listadoHtml = '';
+        for (let student of dataStudent.data.findAllStudents) {
+            let studentRow='<tr><td>'+student.id+'</td><td>' + 
+            student.name + '</td><td>'+student.lastName + '</td><td>' +
+            student.age + '</td><td>' +student.email + '</td><td>'+
+            student.course.name + '</td><td><a class="btn btn-danger btn-circle" onclick=" deleteStudentById('+ student.id +')"><i class="fas fa-trash"></i></a></td>';
+
+            listadoHtml+=studentRow;
+            
+        }
+
+        document.querySelector('#studentTable tbody').outerHTML = listadoHtml;
+      
+       
+    } catch (error) {
+        console.error('Error:', error);
+    }
+
+}
+
+async function loadCourse(){
+
+    try {
+        const query = `
+            query {
+                findAllCourses {
+                    id,
+                    name,
+                    category,
+                    description,
+                    teacher
+                  }
+            }
+        `;
+        
+        const requestCourse = await   fetch('http://localhost:8080/graphql', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            },
+            body: JSON.stringify({ query })
+        })
+        let dataCourse = await requestCourse.json();
+        
+        let listadoHtmlCourse = '';
+        for (let course of dataCourse.data.findAllCourses) {
+            let courseRow='<tr><td>'+course.id+'</td><td>' + course.name + '</td><td>'+course.category + '</td><td>'+course.teacher +'</td><td>' +course.description + '</td>';
+            listadoHtmlCourse+=courseRow;
+        }
+
+        document.querySelector('#dataTableCourse tbody').outerHTML = listadoHtmlCourse;
+      
+       
+    } catch (error) {
+        console.error('Error:', error);
+    }
+
+}
+
+
+
+async function deleteStudentById(studentId) {
+    try {
+        const mutation = `
+            mutation ($studentId: String!) {
+                deleteStudentById(studentId: $studentId)
+            }
+        `;
+
+        const response = await fetch('http://localhost:8080/graphql', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            },
+            body: JSON.stringify({ 
+                query: mutation,
+                variables: { studentId }
+            })
+        });
+
+        const data = await response.json();
+        loadStudents();
+        return data.data.deleteStudentById;
+    } catch (error) {
+        loadStudents();
+        console.error('Error:', error);
+        return null;
+    }
+
+}
+
+async function createStudent(inputStudent) {
+    try {
+        const mutation = `
+            mutation ($inputStudent: InputStudent!) {
+                createStudent(inputStudent: $inputStudent) {
+                    id
+                    name
+                    lastName
+                    age
+                    email
+                    course {
+                        id
+                        name
+                        category
+                        teacher
+                        description
+                    }
+                }
+            }
+        `;
+
+        const response = await fetch('http://localhost:8080/graphql', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            },
+            body: JSON.stringify({ 
+                query: mutation,
+                variables: { inputStudent }
+            })
+        });
+
+        const data = await response.json();
+        console.log(data);
+        return data.data.createStudent;
+    } catch (error) {
+        console.error('Error:', error);
+        return null;
+    }
+}
+
+async function saveStudent(){
+    try {
+
+        let inputStudent={
+            id:undefined,
+            name: undefined,
+            lastName:undefined,
+            age:undefined,
+            email:undefined,
+            courseId : undefined, 
+        };
+
+        const query = `
+            query {
+                findAllCourses {
+                    id,
+                    name,
+                    category,
+                    description,
+                    teacher
+                  }
+            }
+        `;
+        
+        const requestCourse = await   fetch('http://localhost:8080/graphql', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            },
+            body: JSON.stringify({ query })
+        })
+        let dataCourse = await requestCourse.json();
+        
+        
+
+        let nameStudent = document.getElementById('nameStudent').value;
+        let lastNameStudent = document.getElementById('lastNameStudent').value;
+        let age = document.getElementById('age').value;
+        let email=document.getElementById('email').value;
+        let courseId=document.getElementById('courseStudent').value;
+
+        
+        inputStudent.name=nameStudent;
+        inputStudent.lastName=lastNameStudent;
+        inputStudent.age=age;
+        inputStudent.email=email;
+        inputStudent.courseId=courseId;
+        console.log(inputStudent);
+        
+        createStudent(inputStudent)
+        .then(data => {
+            console.log('Estudiante creado:', data);
+            loadStudents();
+        })
+        .catch(error => {
+            console.error('Error al crear estudiante:', error);
+        });
+        
+    } catch (error) {
+        
+    }
+}
+
+async function selectStudent(){
+    try {
+        const query = `
+            query {
+                findAllCourses {
+                    id,
+                    name
+                  }
+            }
+        `;
+        
+        const requestCourse = await   fetch('http://localhost:8080/graphql', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            },
+            body: JSON.stringify({ query })
+        })
+        let cursos = await requestCourse.json();
+       
+        const selectCurso = document.getElementById('courseStudent');
+        
+        let listadoHtmlCourse = '';
+        for (let course of cursos.data.findAllCourses) {
+            const option = document.createElement('option');
+            option.value = course.id;
+            option.textContent = course.name;
+            selectCurso.appendChild(option);
+        }
+
+        
+        
+    } catch (error) {
+        
+    }
+    
+    
+
+}
+
+async function createCourse(inputCourse) {
+    try {
+        const mutation = `
+            mutation ($inputCourse: InputCourse!) {
+                createCourse(inputCourse: $inputCourse) {
+                    id
+                    name
+                    category
+                    teacher
+                    description
+                }
+            }
+        `;
+
+        const response = await fetch('http://localhost:8080/graphql', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            },
+            body: JSON.stringify({ 
+                query: mutation,
+                variables: { inputCourse }
+            })
+        });
+
+        const data = await response.json();
+        return data.data.createCourse;
+    } catch (error) {
+        console.error('Error:', error);
+        return null;
+    }
+}
